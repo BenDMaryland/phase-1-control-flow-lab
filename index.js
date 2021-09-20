e2445e95b75c82366042ad193b3cd621a29078d7
Benjamin <ben.d.maryland@gmail.com>
2021-09-20 13:03:10 -0400

    commit tree d4f363d9e2d69b96ab3a7e89116c5f77e29209ea

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(someNumber){
+  if (someNumber < 200) {
+     return  'This one is on me!'
+     
+} else if (someNumber > 2000 && someNumber < 2500){
+  return 'I will gladly take your thirty bucks.'
+} else {
+  return 'No can do.'
+}
+}
+
+function ternaryCheckCity(cityName){
+  return cityName === 'NYC'?'Ok, sounds good.' :"No go."
+
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+    return  "Thank you."
+    default:
+      return"Bye."
+  }
   // Write your code here!
 }
